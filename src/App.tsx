d5841b6673f6173cc0bfccb0e068cf898e80102b
rkpattnaik780 <rkpattnaik780@gmail.com>
2024-02-13 00:49:13 +0530

    refactor: fix errors and stuffs

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { Box } from './Box';
 function App() {
 
   const [clickedStack, setClickedStack] =  useState<number[][]>([]);
-  const [boxes, setBoxes] = React.useState([
+  const [boxes, setBoxes] = React.useState<boolean[][]>([
     [false, false, false],
     [false, false, false],
     [false, false, false],
@@ -16,41 +16,30 @@ function App() {
     if(clickedStack.length === 7) {
         
       timerID.current = setInterval(() => {
-        console.log("hello");
         let row: number, col: number;
         
         setClickedStack((stack) => {
           let newClickedStack = [...stack];
-          console.log(newClickedStack);
           [row, col] = newClickedStack[newClickedStack.length - 1];
           newClickedStack.pop();
-          console.log(row, col);
           return newClickedStack;
         });
 
         setBoxes((boxes) => {
           let newBoxes = [...boxes];
-          console.log(row, col);
           newBoxes[row][col] = false;
-
-          console.log(newBoxes);
           return newBoxes;
         });
-  
-        console.log(clickedStack);
-      }, 2000);
+        }, 2000);
     }
 
     if(clickedStack.length === 0) {
-      console.log(timerID.current);
-      console.log("I hit 0")
       clearInterval(timerID.current);
     }
 
   }, [clickedStack]);
 
   const updateBox = (row: number, col: number) => {
-    console.log("update box called");
     let newBoxes = [...boxes]
     if(newBoxes[row][col] === false) {
        setClickedStack([...clickedStack, [row, col]]);
@@ -64,10 +53,10 @@ function App() {
       {
         boxes.map((row, rowIndex) => {
           return (
-            <div>
+            <div key={rowIndex}>
               {row.map((isGreen, colIndex) => {
-                if(rowIndex == 1 && (colIndex === 1 || colIndex === 2)) return <Box row={rowIndex} col={colIndex} isHidden={true} updateBox={updateBox} key={rowIndex + colIndex} isGreen={isGreen}/>
-                return <Box row={rowIndex} col={colIndex} isGreen={isGreen} updateBox={updateBox} key={rowIndex + colIndex}/>
+                if(rowIndex === 1 && (colIndex === 1 || colIndex === 2)) return <Box row={rowIndex} col={colIndex} isHidden={true} updateBox={updateBox} key={rowIndex + "-" + colIndex} isGreen={isGreen}/>
+                return <Box row={rowIndex} col={colIndex} isGreen={isGreen} updateBox={updateBox} key={rowIndex + "-" + colIndex}/>
               })}
               <br />
             </div>
@@ -75,7 +64,6 @@ function App() {
           )
         })
       }
-      
     </div>
   );
 }
--- src/Box.tsx
+++ src/Box.tsx
@@ -1,5 +1,3 @@
-import { useState } from "react";
-
 interface IBox {
     isGreen: number | boolean;
     row: number;
@@ -12,8 +10,16 @@ export const Box: React.FC<IBox> = ({ isGreen, updateBox, row, col, isHidden }) 
 
     return (
         <div 
-            style={{ width: "100px", height: "100px", border: "1px solid black", display: "inline-block", backgroundColor: isGreen ? "green" : "initial", visibility: isHidden ? "hidden" : "initial" }}
+            style={{
+                width: "100px",
+                height: "100px",
+                border: "1px solid black",
+                display: "inline-block",
+                backgroundColor: isGreen ? "green" : "initial",
+                visibility: isHidden ? "hidden" : "initial",
+                padding: "5px"
+            }}
             onClick={() => updateBox(row, col)}
-        >Hello</div>
+        />
     )
 }
