e21a2b6afb84dba66a7191c60a2d074c504361a1
rkpattnaik780 <rkpattnaik780@gmail.com>
2024-02-13 00:32:25 +0530

    first implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
 import { Box } from './Box';
 
 function App() {
 
+  const [clickedStack, setClickedStack] =  useState<number[][]>([]);
   const [boxes, setBoxes] = React.useState([
-    [[0,0, false], [0,1, false], [0,2, false]],
-    [[1,0, false], [1,1, false], [1,2, false]],
-    [[2,0, false], [2,1, false], [2,2, false]],
+    [false, false, false],
+    [false, false, false],
+    [false, false, false],
   ]);
+  const timerID = useRef<NodeJS.Timer>();
 
+  useEffect(() => {
+    if(clickedStack.length === 7) {
+        
+      timerID.current = setInterval(() => {
+        console.log("hello");
+        let row: number, col: number;
+        
+        setClickedStack((stack) => {
+          let newClickedStack = [...stack];
+          console.log(newClickedStack);
+          [row, col] = newClickedStack[newClickedStack.length - 1];
+          newClickedStack.pop();
+          console.log(row, col);
+          return newClickedStack;
+        });
 
+        setBoxes((boxes) => {
+          let newBoxes = [...boxes];
+          console.log(row, col);
+          newBoxes[row][col] = false;
+
+          console.log(newBoxes);
+          return newBoxes;
+        });
+  
+        console.log(clickedStack);
+      }, 2000);
+    }
+
+    if(clickedStack.length === 0) {
+      console.log(timerID.current);
+      console.log("I hit 0")
+      clearInterval(timerID.current);
+    }
+
+  }, [clickedStack]);
+
+  const updateBox = (row: number, col: number) => {
+    console.log("update box called");
+    let newBoxes = [...boxes]
+    if(newBoxes[row][col] === false) {
+       setClickedStack([...clickedStack, [row, col]]);
+    }
+    newBoxes[row][col] = true;
+    setBoxes(newBoxes);
+  };
 
   return (
     <div className="App">
-
       {
-        boxes.map(row => {
+        boxes.map((row, rowIndex) => {
           return (
             <div>
-              {row.map(col => {
-                if(col[0] == 1 && (col[1] == 1 || col[1] == 2)) return <Box hidden={true} isGreen={col[2]} coordinate={[col[0], col[1]]} />
-                return <Box coordinate={[col[0], col[1]]} isGreen={col[2]} setBox={setBoxes} box={boxes}/>
+              {row.map((isGreen, colIndex) => {
+                if(rowIndex == 1 && (colIndex === 1 || colIndex === 2)) return <Box row={rowIndex} col={colIndex} isHidden={true} updateBox={updateBox} key={rowIndex + colIndex} isGreen={isGreen}/>
+                return <Box row={rowIndex} col={colIndex} isGreen={isGreen} updateBox={updateBox} key={rowIndex + colIndex}/>
               })}
               <br />
             </div>
--- src/Box.tsx
+++ src/Box.tsx
@@ -1,27 +1,19 @@
 import { useState } from "react";
 
 interface IBox {
-    hidden?: boolean;
     isGreen: number | boolean;
-    coordinate: (boolean | number)[];
-    setBox? :React.Dispatch<React.SetStateAction<(number | boolean)[][][]>>;
-    box? : (number | boolean)[][][];
+    row: number;
+    col:number;
+    updateBox : (row: number, col: number) => void;
+    isHidden?: boolean;
 }
 
-export const Box: React.FC<IBox> = ({ hidden, isGreen, setBox, box, coordinate }) => {
-
-    // const [isGreen, setIsGreen] = useState<boolean>(false);
-
-    const clickBox = () => {
-        console.log("clicked");
-        box[coordinate[0]][coordinate[1]]
-        setBox()
-    }
+export const Box: React.FC<IBox> = ({ isGreen, updateBox, row, col, isHidden }) => {
 
     return (
         <div 
-            style={{ width: "100px", height: "100px", border: "1px solid black", display: "inline-block", visibility: hidden ? "hidden" : "initial", backgroundColor: isGreen ? "green" : "initial" }}
-            onClick={clickBox}
+            style={{ width: "100px", height: "100px", border: "1px solid black", display: "inline-block", backgroundColor: isGreen ? "green" : "initial", visibility: isHidden ? "hidden" : "initial" }}
+            onClick={() => updateBox(row, col)}
         >Hello</div>
     )
 }
